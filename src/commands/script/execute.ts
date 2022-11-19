9a614b740a5cce9a52d67af31b71b37eff95dac3
lmnch <lukas.moench@tech11.com>
2022-11-19 11:05:04 +0100

    added first script execution command

--- src/commands/script/execute.ts
+++ src/commands/script/execute.ts
@@ -1,5 +1,7 @@
 import { Command } from "@oclif/core";
 import LRClient from "../../boundary/LRestClient";
+import * as vm from "node:vm";
+import * as fs from "fs/promises";
 
 export default class Execute extends Command {
 
@@ -15,7 +17,13 @@ export default class Execute extends Command {
         const client = new LRClient();
         await client.init();
 
-        const script =  (await import(args.script)).default();
-        script(client);
+
+        const script = (await fs.readFile(args.script)).toString();
+        // Wrap inside of method to support await keyword
+        const code = `const executionLrcMethod = async (lrc) => { ${script} };\nexecutionLrcMethod(lrc);` 
+        const context = {lrc: client};
+        vm.createContext(context);
+
+        vm.runInContext(code, context);
     }
 }
