1699cd3f93fb1484475345afa0554c4dc9ffdf66
lmnch <lukas.moench@tech11.com>
2022-11-19 13:48:42 +0100

    added some description

--- src/commands/script/execute.ts
+++ src/commands/script/execute.ts
@@ -1,29 +1,30 @@
 import { Command } from "@oclif/core";
-import LRClient from "../../boundary/LRestClient";
+import LRClient from "../../boundary/LRClient";
 import * as vm from "node:vm";
 import * as fs from "fs/promises";
 
 export default class Execute extends Command {
+    static description = `Executes a ECMA script by providing LRClient as "lrc" variable in the context of an async method (=> "await" can be used).`;
 
+    static examples = [`<%= config.bin %> <%= command.id %> ./scripts/testscript.js
+    `];
 
     static args = [
         { name: 'script', description: "Path to script that should be executed", required: true }
     ]
 
     async run(): Promise<void> {
-        const { args, flags } = await this.parse(Execute);
+        const { args } = await this.parse(Execute);
 
         // Dependeincies
         const client = new LRClient();
         await client.init();
-
 
         const script = (await fs.readFile(args.script)).toString();
         // Wrap inside of method to support await keyword
         const code = `const executionLrcMethod = async (lrc) => { ${script} };\nexecutionLrcMethod(lrc);` 
         const context = {lrc: client};
         vm.createContext(context);
-
         vm.runInContext(code, context);
     }
 }
