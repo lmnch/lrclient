6edfe30f9d55bb219cab953f8f8b4a20cb5a40a0
lmnch <lukas.moench@tech11.com>
2022-11-24 17:51:54 +0100

    added http methods

--- src/model/HttpMethod.ts
+++ src/model/HttpMethod.ts
@@ -1,6 +1,6 @@
 
 enum HttpMethod {
-    GET="GET", POST="POST"
+    GET="GET", POST="POST", PUT="PUT", DELETE="DELETE", PATCH="PATCH", HEAD="HEAD", CONNECT="CONNECT", TRACE="TRACE"
 }
 
 export default HttpMethod;
